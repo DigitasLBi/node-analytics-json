e81a00690a52e7b53d1a121e2aaae9c7e3c6c1a4
Adam Balchunas <adam.balchunas@digitas.com>
2014-04-12 18:57:15 -0400

    Added clean and CSV to clean JSON conversion functions

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ dtas.json = (function() {
           clean(array[index + 1]);
           break;
 
+        case '-v':
+          convert(array[index + 1]);
+          break;
+
         case '-h':
           help();
           break;
@@ -22,15 +26,61 @@ dtas.json = (function() {
     });
   }
 
+  function convert(file) {
+    var Converter = require("csvtojson").core.Converter;
+    var csvConverter = new Converter();
+
+    csvConverter.on("end_parsed", function(jsonObj) {
+      cleanUpJSON('temp.json', jsonObj, false);
+    });
+
+    csvConverter.from(file);
+  }
+
   function clean(file) {
+    var data = readJSON(file, cleanUpJSON);
+  }
+
+  function cleanUpJSON(file, data, parse) {
+    data = (parse === true) ? removeNulls(JSON.parse(data)) : removeNulls(data);
+    
+    writeJSON(file + '.tmp', data);
+
+    function removeNulls(obj) {
+      var isArray = obj instanceof Array;
+      
+      for (var k in obj) {
+        if (obj[k] === null || obj[k] === 'null' || obj[k] === '') {
+          isArray ? obj.splice(k, 1) : delete obj[k];
+        }
+        else if (typeof obj[k] === "object") {
+          removeNulls(obj[k]);
+        }
+      }
+    
+      return obj;
+    }
+  }
+
+  function readJSON(file, callback) {
     fs.readFile(file, 'utf8', function (err, data) {
       if (err) {
         console.log('Error: ' + err);
         return;
       }
- 
-      data = JSON.parse(data);
-      console.dir(data);
+
+      callback(file, data, true);
+    });
+  }
+
+  function writeJSON(path, data) {
+    fs.writeFile(path, JSON.stringify(data, null, 2), function(err) {
+      if (err) {
+        console.log(err);
+      } 
+      else {
+        console.log("The file was saved!");
+      }
     });
   }
 
